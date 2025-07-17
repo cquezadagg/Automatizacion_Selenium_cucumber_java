da3bc6e72f7fdd7a8be219425c3defc75d9f5a79
cquezada <metalpark85@gmail.com>
2025-07-17 11:41:28 -0400

    Agrega capturas de pantalla de las pruebas y actualiza las credenciales de usuario

--- src/test/java/steps/MainSteps.java
+++ src/test/java/steps/MainSteps.java
@@ -13,6 +13,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
@@ -47,21 +48,28 @@ public class MainSteps {
         extent = ExtentReportManager.getInstance();
     }
 
-@Before
-public void beforeScenario(Scenario scenario) {
-    scenarioTest = extent.createTest(scenario.getName());
+    @Before
+    public void beforeScenario(Scenario scenario) {
+        scenarioTest = extent.createTest(scenario.getName());
 
-    if (driver == null) {
-        if (OS.isName("Windows")) {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver.exe");
-        } else if (OS.isName("Linux")) {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/driver/chromedriver");
+        if (driver == null) {
+            if (OS.isName("Windows")) {
+                System.setProperty("webdriver.edge.driver", "src/test/resources/driver/msedgedriver.exe");
+            } else if (OS.isName("Linux")) {
+                System.setProperty("webdriver.edge.driver", "src/test/resources/driver/msedgedriver");
+            }
+ChromeOptions options = new ChromeOptions();
+options.addArguments("--headless=new");
+options.addArguments("--no-sandbox");
+options.addArguments("--disable-dev-shm-usage");
+options.addArguments("--disable-gpu");
+options.addArguments("--window-size=1920,1080");
+options.addArguments("--remote-allow-origins=*");
+
+driver = new ChromeDriver(options);
+
         }
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
     }
-}
-
 
  @AfterStep
 public void afterStep(Scenario scenario) {
